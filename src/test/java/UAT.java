f8af788e920bd0a143fafb1c445ef49784235837
Berglurch <empty>
2018-06-25 13:25:05 +0200

    ??

--- src/test/java/UAT.java
+++ src/test/java/UAT.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class UntitledTestCase {
+public class UAT {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
