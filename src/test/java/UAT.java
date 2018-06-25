1c1f5ff0f329b1a4329dbeadf1fcd01a0640e992
Berglurch <empty>
2018-06-25 15:10:47 +0200

    ?

--- src/test/java/UAT.java
+++ src/test/java/UAT.java
@@ -30,6 +30,7 @@ public class UAT {
   @Test
   public void testUAT() throws Exception {
     driver.get("http://192.168.50.105:8080/");
+    Thread.sleep(5000);
     driver.findElement(By.id("cfg-size-length")).click();
     driver.findElement(By.id("cfg-size-length")).clear();
     driver.findElement(By.id("cfg-size-length")).sendKeys("10");
