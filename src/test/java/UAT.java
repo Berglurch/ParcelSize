e42c8420ce834e15e6cafac4e4a7f69a7c2bf7d8
reen <reen@Reen-Laptop>
2018-07-02 11:21:32 +0200

    ?

--- src/test/java/UAT.java
+++ src/test/java/UAT.java
@@ -38,7 +38,7 @@ public class UAT {
 
   @Test
   public void testUAT() throws Exception {
-    Thread.sleep(5000);
+    Thread.sleep(500000000);
 	driver.get("http://192.168.50.105:8080/");
     driver.findElement(By.id("cfg-size-length")).click();
     driver.findElement(By.id("cfg-size-length")).clear();
